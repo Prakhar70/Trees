db2c5d592045267261ac54a9728c260dbb324d5f
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-09-18 16:47:19 +0530

    TreeNode created

---
+++ TreeNode.java
@@ -0,0 +1,11 @@
+class TreeNode {
+    int val;
+    TreeNode right;
+    TreeNode left;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
